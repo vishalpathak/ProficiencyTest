644436af4fcfdbfaed00c7d2649d15873c06575e
vishal <vishal.p@borqs.com>
2020-03-21 16:08:40 +0530

    RootViewController setup -- Set up Root view controllr for ios 13 and ios 12 -- info.plist setting update for request

--- ProficiencyTest/AppDelegate.swift
+++ ProficiencyTest/AppDelegate.swift
@@ -11,10 +11,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 11.0, *) {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let homeView = HomeViewController()
+            let navigationView = UINavigationController(rootViewController: homeView)
+            window?.rootViewController = navigationView
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
